32dbb82fc728fa7d66750679e7b9dc829209bfbf
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-04-24 11:18:57 -0400

    Terminal not opening

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 
 #APT Cleaner
 sleep 5
-lxterminal --title="APT Cleaner" --command="sudo apt update -y && sudo apt autoremove -y && sudo apt clean -y && echo 'System has been cleaned'"
+lxterminal --title="APT Cleaner" --command="sudo apt autoremove -y"
