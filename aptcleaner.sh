e0272edb599dba0981197c9d7a3f45eec6606447
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-04-24 11:52:45 -0400

    Fixed script running issue

--- aptcleaner.sh
+++ aptcleaner.sh
@@ -2,4 +2,4 @@
 
 #APT Cleaner
 sleep 5
-lxterminal --title="APT Cleaner" --command="/bin/bash -c './clean.sh; sleep 10'"
+lxterminal --title="APT Cleaner" --command="/bin/bash -c 'cd "$HOME"/aptcleaner; ./clean.sh; sleep 10'"
